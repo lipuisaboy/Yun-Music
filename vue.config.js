2c19971ec900db0d721026bb93ad9666c33d952e
lipuisaboy <1213262105@qq.com>
2020-03-05 04:43:32 +0800

    部署

---
+++ vue.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  publicPath:'./',
+  devServer:{
+    open:true,
+    host:'localhost',
+    port:8080,
+    https:false,
+    hotOnly:false,
+    proxy:{
+      '/api':{
+        target:'http://yangxc.cn:7001/test-api',
+        ws:true,
+        changOrigin:true,
+        pathRewrite:{
+          '^/api':'/'
+        }
+      }
+    }
+  }
+}
