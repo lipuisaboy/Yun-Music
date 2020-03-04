4921f4ab9135ca0efe2dd14d076583a6bf96c2d8
lipuisaboy <1213262105@qq.com>
2020-03-05 05:15:32 +0800

    部署完成

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,5 @@
 module.exports = {
-  publicPath:'./',
-  aevServer:{
-    open:true,
-    host:"localhost",
-    port:8080,
-    https:false,
-    hotOnly:false,
-    proxy:{
-      '/api':{
-        target:"http://yangxc.cn:7001/test-api",
-        ws:true,
-        changOrigin:true,
-        pathRewrite:{
-          '^/api':'/'
-        }
-      }
-    }
-  }
+  publicPath: process.env.NODE_ENV === 'production'
+  ? './'
+  : '/'
 }
